fd3f5a1f7c96a789b7ad955207eec4e3f03f1c4f
Dhiraj Moudgil <dmoudgil@bebotechnologies.com>
2019-07-19 15:03:57 +0530

    Commiting final Application

--- models/users.js
+++ models/users.js
@@ -23,9 +23,24 @@ var User = module.exports = mongoose.model('users', UserSchema);
 
 module.exports.createUser = function(newUser, callback){
 	bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(newUser.password, salt, function(err, hash) {
-        newUser.password = hash;
-        newUser.save(callback);
-    });
-});
+		bcrypt.hash(newUser.password, salt, function(err, hash) {
+		    newUser.password = hash;
+		    newUser.save(callback);
+		});
+	});
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	User.findOne({username: username}, callback);
+}
+
+module.exports.comparePassword = function(candidatePasssword, hash, callback){
+	bcrypt.compare(candidatePasssword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findOne({_id: id}, callback);
 }
